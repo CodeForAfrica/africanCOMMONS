89296f83f2537530f2708e0771c6a1e196605d53
David Lemayian <dlemayian@gmail.com>
2017-12-22 15:06:30 +0300

    Fix with standard.

--- src/js/plugins.js
+++ src/js/plugins.js
@@ -25,60 +25,56 @@ $(function () {
 
 // Place any jQuery/helper plugins in here.
 $(function () {
-
 // Enable bootstrap tooltips
 
   $('[data-toggle="tooltip"]').tooltip()
 
-  $('.pattern').on('click', function(e){
-
+  $('.pattern').on('click', function (e) {
     // Facebook App ID config
-    var fbAppId = '200110850534528';
+    var fbAppId = '200110850534528'
 
-    var url = 'https://commons.africa/';
-    var redirectUri = 'https://commons.africa/';
-    var VanillaSharing  = require('vanilla-sharing');
+    var url = 'https://commons.africa/'
+    var redirectUri = 'https://commons.africa/'
+    var VanillaSharing = require('vanilla-sharing')
 
-    if (location.hostname === "localhost" || location.hostname === "127.0.0.1"){
-      redirectUri = 'http://localhost:4000/';
+    if (window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1') {
+      redirectUri = 'http://localhost:4000/'
     }
 
-    var network = $(this).data('pattern-network');
-    var title = $(this).data('pattern-title');
-    var tags = $(this).data('pattern-tags');
-    var tagline = $(this).data('pattern-tagline');
-    tags = '#' + tags;
+    var network = $(this).data('pattern-network')
+    var title = $(this).data('pattern-title')
+    var tags = $(this).data('pattern-tags')
+    // var tagline = $(this).data('pattern-tagline')  // Unused
+    tags = '#' + tags
 
-    tags = ' #civicpatterns #africanCOMMONS #commons';
+    tags = ' #civicpatterns #africanCOMMONS #commons'
 
     switch (network) {
-
       case 'facebook-share':
-          VanillaSharing.fbShare({
-            url: url,
-            redirectUri: redirectUri,
-            hashtag: tags,
-            quote: title,
-            fbAppId: fbAppId 
-          });
-        break;
+        VanillaSharing.fbShare({
+          url: url,
+          redirectUri: redirectUri,
+          hashtag: tags,
+          quote: title,
+          fbAppId: fbAppId
+        })
+        break
       case 'facebook-like':
-          VanillaSharing.fbFeed({
-            url: url,
-            redirectUri: redirectUri,
-            fbAppId: fbAppId
-          });
-      break;
+        VanillaSharing.fbFeed({
+          url: url,
+          redirectUri: redirectUri,
+          fbAppId: fbAppId
+        })
+        break
       case 'twitter':
-          VanillaSharing.tw({
-            url: url,
-            title: title + tags,
-        });
-      break;
+        VanillaSharing.tw({
+          url: url,
+          title: title + tags
+        })
+        break
       default:
     }
 
-    return false;
-    });
-
+    return false
+  })
 })
